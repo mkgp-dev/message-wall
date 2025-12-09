f6b96c2e694c0fc23a92e5d3ea509e3e07f44d13
Mark Pelayo <mark.pelayo014@gmail.com>
2025-12-09 22:17:09 +0800

    hotfix: view directory

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import messageRouter from './routes/messageRouter';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
-const viewsPath = path.join(__dirname, "views");
+const viewsPath = path.join(__dirname, "../views");
 const publicPath = path.join(__dirname, "../public");
 
 app.set("view engine", "ejs");
